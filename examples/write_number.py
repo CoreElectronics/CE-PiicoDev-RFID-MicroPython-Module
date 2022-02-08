a1a93e099ebfa1b5a94659692ae98275652fb82b
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-02-08 18:23:56 +1100

    Create write_number.py

---
+++ examples/write_number.py
@@ -0,0 +1,19 @@
+from PiicoDev_RFID import PiicoDev_RFID
+from PiicoDev_Unified import sleep_ms
+
+rfid = PiicoDev_RFID()
+
+slot = 35
+number = 1
+
+print('Place tag near the PiicoDev RFID Module')
+print('')
+
+while True:
+    success = rfid.writeNumber(number, slot=slot)
+    if success:
+        readNumber = rfid.readNumber(slot=slot)
+        print('Number in tag:')
+        print(readNumber)
+        break
+    sleep_ms(10)
