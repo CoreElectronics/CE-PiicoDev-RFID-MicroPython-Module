98cc4e71b0acdfec04501ffeabe1d87da455651f
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-02-08 18:23:32 +1100

    Create read_number.py

---
+++ examples/read_number.py
@@ -0,0 +1,17 @@
+from PiicoDev_RFID import PiicoDev_RFID
+from PiicoDev_Unified import sleep_ms
+
+rfid = PiicoDev_RFID()
+
+slot = 0
+
+print('Place tag near the PiicoDev RFID Module')
+print('')
+
+while True:
+    if rfid.tagPresent():
+        read_number = rfid.readNumber(slot=slot)
+        print('Number in tag:')
+        print(read_number)
+        sleep_ms(1000)
+    sleep_ms(10)
