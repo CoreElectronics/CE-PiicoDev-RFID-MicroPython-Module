5e21c7de73d704670d24ff96b5896442873a108f
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-02-08 11:02:08 +1100

    Simplify

--- examples/read_text.py
+++ examples/read_text.py
@@ -1,13 +1,13 @@
 from PiicoDev_RFID import *
 
 rfid = PiicoDev_RFID()
-register = 8
 
 print('Place tag near the PiicoDev RFID Module')
 print('')
 
 while True:
-    text = rfid.readTextFromTag()
-    print('Text in tag ', end=''); print(register, end=''); print(': ', end=''); print(tag_text)
-    print()
+    text_read = rfid.readTextFromTag()
+    print('Text in tag:')
+    print(text_read)
+    break
     sleep_ms(1000)
