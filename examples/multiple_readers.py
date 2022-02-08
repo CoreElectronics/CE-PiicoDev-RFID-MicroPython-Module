331e0766a981a259e0e50ea10064386fa0ea1184
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-02-09 10:15:47 +1100

    Create multiple_readers.py

---
+++ examples/multiple_readers.py
@@ -0,0 +1,19 @@
+from PiicoDev_RFID import PiicoDev_RFID
+from PiicoDev_Unified import sleep_ms
+
+readerA = PiicoDev_RFID()                 # Initialise the first RFID module as reader A with default address 0x2D
+readerB = PiicoDev_RFID(address=0x2D)     # Initialise the second RFID module as reader B
+
+print('Place tag near one of the PiicoDev RFID Modules')
+print('')
+
+while True:    
+    if readerA.tagPresent():              # if an RFID tag is present on reader A
+        id = readerA.readId()             # get the id
+        print('Reader A ID: ' + str(id))  # print the id
+        
+    if readerB.tagPresent():              # if an RFID tag is present on reader B
+        id = readerB.readId()             # get the id
+        print('Reader B ID: ' + str(id))  # print the id
+
+    sleep_ms(500)
