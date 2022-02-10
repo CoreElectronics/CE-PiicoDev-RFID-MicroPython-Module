dabc882cd02ba205bdb33d54450c3871a89fe60b
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-02-10 16:40:57 +1100

    Create write_url.py

---
+++ examples/write_url.py
@@ -0,0 +1,19 @@
+from PiicoDev_RFID import PiicoDev_RFID
+from PiicoDev_Unified import sleep_ms
+
+rfid = PiicoDev_RFID()
+
+print('Place tag near the PiicoDev RFID Module')
+print('')
+
+url = 'www.core-electronics.com.au'
+
+while True:
+    if rfid.tagPresent():
+        success = rfid.writeURL(url)
+        if success:
+            text_read = rfid.readText()
+            print('Text in tag:')
+            print(text_read)
+            break
+    sleep_ms(10)
